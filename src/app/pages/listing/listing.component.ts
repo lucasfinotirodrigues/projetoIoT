88f5b921f0a4b3a6cf80028c81e07ee76330605d
Lucas Finoti Rodrigues <lucas-finoti@lucas.rodrigues>
2024-09-03 20:28:43 -0300

    feat: trazendo dados para o listing

--- src/app/pages/listing/listing.component.ts
+++ src/app/pages/listing/listing.component.ts
@@ -1,37 +1,33 @@
-// listing.component.ts
-
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
-  styleUrls: ['./listing.component.scss']
+  styleUrls: [
+    './listing.component.scss',
+  ],
 })
 export class ListingComponent implements OnInit {
-  lista: any[] = [];
-  private apiUrl = 'https://api.thingspeak.com/channels/2141966/feeds.json?api_key=EX1VAR2WW3758ER4&results=2';
+  channel: any;
+  feeds: any[] = [];
 
-  constructor(private http: HttpClient, private router: Router) {} // Injeta o Router
+  constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    // Faz a requisição para a API
-    this.http.get<any>(this.apiUrl).subscribe(data => {
-      // Manipula o JSON conforme sua necessidade
-      this.lista = data.feeds.map((feed: { field1: any; created_at: string | number | Date; field2: any; }) => ({
-        estufa: data.channel.name,
-        temp: feed.field1 || 'N/A',
-        horario: new Date(feed.created_at).toLocaleTimeString(),
-        umidade: feed.field2 || 'N/A',
-        localizacao: `${data.channel.latitude}, ${data.channel.longitude}`
-      }));
-    });
+    this.getAPI();
   }
 
-
-  selecionarEstufa(estufa: string): void {
-    // Navega para o componente de dashboard passando o nome da estufa como parâmetro
-    this.router.navigate(['/dashboard'], { queryParams: { estufa } });
+  getAPI() {
+    this.apiService.getAPI().subscribe(
+      (data) => {
+        console.warn('Dados da API:', data);
+        this.channel = data.channel;
+        this.feeds = data.feeds;
+      },
+      (error) => {
+        console.error('Erro ao consumir a API:', error);
+      }
+    );
   }
 }
