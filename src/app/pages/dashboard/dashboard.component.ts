739a0ad306a752670dd93b6d076120a903d00554
Lucas Finoti Rodrigues <lucas-finoti@lucas.rodrigues>
2024-09-08 16:45:54 -0300

    feat: função exportar xlsx

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-dashboard',
@@ -28,4 +29,26 @@ export class DashboardComponent implements OnInit {
       }
     );
   }
+
+  exportToExcel(): void {
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.feeds, {
+      header: ['field1', 'field2', 'created_at']
+    });
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Feeds');
+
+    // Generate buffer
+    const excelBuffer: any = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+
+    // Create a blob and download it
+    const data: Blob = new Blob([excelBuffer], { type: EXCEL_TYPE });
+    const fileName: string = 'horta.xlsx';
+    const downloadLink = document.createElement('a');
+    downloadLink.href = URL.createObjectURL(data);
+    downloadLink.download = fileName;
+    downloadLink.click();
+  }
 }
+
+// Constants for file types
+const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
